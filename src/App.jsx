59c2a02195edea9b43575025197ec2b732dac587
devansh-kc <devansh.kc96@gmail.com>
2024-03-12 13:29:36 +0530

    initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Header, Footer } from "./Components/index  ";
+import Footer from "../src/Components/Footer/Footer";
+import Header from "../src/Components/Header/Header"
+const App = () => {
+  return (
+    <div>
+      <Header />
+      App
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
